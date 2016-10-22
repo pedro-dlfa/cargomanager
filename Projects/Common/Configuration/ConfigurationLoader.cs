5329fc872c1410775d4402bc9c11429e604f5f72
peter <pedro.de.la.fuente@hotmail.com>
2016-10-22 19:39:12 +0200

    Missing files

---
+++ Projects/Common/Configuration/ConfigurationLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+
+namespace CargoManager.Common.Configuration
+{
+    public class ConfigurationLoader
+    {
+        /// <summary>
+        /// Loads a configuration section with a specific name.
+        /// </summary>
+        /// <typeparam name="TSection">The type of the section.</typeparam>
+        /// <param name="sectionName">Name of the section.</param>
+        /// <returns>
+        /// Configuration section
+        /// </returns>
+        /// <exception cref="System.ArgumentException">Value cannot be null or whitespace.;sectionName</exception>
+        public TSection LoadConfigurationSection<TSection>(string sectionName)
+        {
+            if (string.IsNullOrWhiteSpace(sectionName))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", "sectionName");
+            }
+
+            TSection section = (TSection)ConfigurationManager.GetSection(sectionName);
+            return section;
+        }
+    }
+}
---
+++ Projects/Common/IOC/MapperModule.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Ninject;
+using Ninject.Activation;
+using Ninject.Modules;
+
+namespace CargoManager.Common.IOC
+{
+    public class MapperModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<MapperConfiguration>().ToMethod(BuildConfiguration).InSingletonScope();
+
+            this.Bind<IMapper>().ToMethod(c => c.Kernel.Get<MapperConfiguration>().CreateMapper());
+        }
+
+        private static MapperConfiguration BuildConfiguration(IContext context)
+        {
+            var configuration = new MapperConfiguration(cfg => ConfigureMapper(context, cfg));
+            configuration.AssertConfigurationIsValid();
+
+            return configuration;
+        }
+
+        private static void ConfigureMapper(IContext context, IMapperConfigurationExpression cfg)
+        {
+            foreach (var profile in context.Kernel.GetAll<Profile>())
+            {
+                cfg.AddProfile(profile);
+            }
+        }
+    }
+}
